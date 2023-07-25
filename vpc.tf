25d80297c58aefed1b8e53ec576b30c13ae11a6d
Mukesh <Stunty@007>
2023-07-25 21:42:32 +0530

    learning devops

--- vpc.tf
+++ vpc.tf
@@ -6,9 +6,9 @@ resource "aws_vpc" "main" {
 
 module "additional_cidr_block" {
   for_each              = var.vpc
-  source                = "./additional_cidr_block"
+  source                = "./additional_vpc_cidr"
   additional_cidr_block = each.value.additional_cidr_block
-  vpc_id                = [ for k, v in aws_vpc.main : v.id ]
+  vpc_id                = [for k, v in aws_vpc.main : v.id]
 }
 
 output "vpc" {
