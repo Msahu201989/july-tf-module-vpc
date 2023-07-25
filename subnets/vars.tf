6af546969a2ed2f0a9ffa2886931fc8309e75369
Mukesh <Stunty@007>
2023-07-25 21:36:45 +0530

    learning devops

--- subnets/vars.tf
+++ subnets/vars.tf
@@ -1,15 +1,2 @@
-variable "cidr_block" {}
+variable "subnets" {}
 variable "vpc_id" {}
-variable "env" {}
-variable "name" {}
-variable "subnet_availability_zones" {}
-variable "gateway_id" {}
-
-
-#variable "subnets" {}
-#variable "vpc_id" {}
-#variable "env" {}
-#variable "subnet_availability_zones" {}
-#variable "peering_connection_id" {}
-#variable "management_vpc" {}
-#variable "internet_gateway_id" {}
