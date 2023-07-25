c8a34ea83ea69766ecb637528d95ab961c0502e2
Mukesh <Stunty@007>
2023-07-25 21:44:51 +0530

    learning devops

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,5 @@
-module "subnets" {
-  for_each = var.vpc
-  source   = "./subnets"
-  subnets  = each.value.subnets
-  vpc_id   = [for k, v in aws_vpc.main : v.id]
+source   = "./subnets"
+subnets  = each.value.subnets
+vpc_id   = [for k, v in aws_vpc.main : v.id]
+env      = var.env
 }
