4963554655bb731ff049eef53abfe8cd2ad9f083
Mukesh <Stunty@007>
2023-07-26 20:16:13 +0530

    learning devops

--- locals.tf
+++ locals.tf
@@ -7,12 +7,11 @@ locals {
 
 
 
-#  vpc_peering_tags = {
-#    Name    = "${var.env}-vpc-to-management-vpc"
-#    ENV     = var.env
-#    PROJECT = "roboshop"
-#  }
-#
-#  all_route_tables = concat([for i, j in module.private_subnets : j.route_tables.id], [for i, j in module.public_subnets : j.route_tables.id])
-#
+  vpc_peering_tags = {
+    Name    = "${var.env}-vpc-to-management-vpc"
+    ENV     = var.env
+    PROJECT = "roboshop"
+  }
+   vpc_ids = [for k, v in aws_vpc.main : v.id]
 }
+
