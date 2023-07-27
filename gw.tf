84e36276caf30cd4a4b18bb793d6838a2ae46588
Mukesh <Stunty@007>
2023-07-27 20:41:41 +0530

    July 23 learning devops

--- gw.tf
+++ gw.tf
@@ -10,27 +10,27 @@ resource "aws_eip" "ngw" {
   vpc = true
 }
 
-#resource "aws_nat_gateway" "ngw" {
-#  allocation_id = aws_eip.ngw.id
-#  subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
-#  tags = {
-#    Name = "NAT GW"
-#  }
-#}
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+  tags = {
+    Name = "NAT GW"
+  }
+}
 
 
 
-#resource "aws_route" "internet_gateway_route_to_public_subnets" {
-#  count                  = length(local.public_route_tables)
-#  route_table_id         = element(local.public_route_tables, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = aws_internet_gateway.gw[0].id
-#}
+resource "aws_route" "internet_gateway_route_to_public_subnets" {
+  count                  = length(local.public_route_tables)
+  route_table_id         = element(local.public_route_tables, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw[0].id
+}
 
-#
-#resource "aws_route" "nat_gateway_route_to_public_subnets" {
-#  count                  = length(local.private_route_tables)
-#  route_table_id         = element(local.private_route_tables, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = aws_nat_gateway.ngw[0].id
-#}
+
+resource "aws_route" "nat_gateway_route_to_public_subnets" {
+  count                  = length(local.private_route_tables)
+  route_table_id         = element(local.private_route_tables, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.ngw[0].id
+}
