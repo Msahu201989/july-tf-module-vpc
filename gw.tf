b2288db152f77b3fa3d6934d4ea94e291cc33850
Mukesh <Stunty@007>
2023-07-27 21:56:43 +0530

    July 23 learning devops

--- gw.tf
+++ gw.tf
@@ -1,6 +1,6 @@
-resource "aws_internet_gateway" "igw" {
-  count = length(local.vpc_ids)
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "gw" {
+  count  = length(local.vpc_ids)
+  vpc_id = element(local.vpc_ids, count.index)
 
   tags = {
     Name = "${var.env}-igw"
@@ -12,15 +12,21 @@ resource "aws_eip" "ngw" {
 }
 
 resource "aws_nat_gateway" "ngw" {
-  count = length(local.vpc_ids)
+  count         = length(local.vpc_ids)
   allocation_id = aws_eip.ngw.id
-  subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+  subnet_id     = local.public_subnets_list[0]
+
   tags = {
     Name = "NAT GW"
   }
 }
 
-
+locals {
+  private_route_tables = flatten([for i, j in module.private_subnets : j.rt])
+  private_subnets_list = flatten([for i, j in module.private_subnets : j.subnets_list])
+  public_subnets_list  = flatten([for i, j in module.public_subnets : j.subnets_list])
+  public_route_tables  = flatten([for i, j in module.public_subnets : j.rt])
+}
 
 resource "aws_route" "internet_gateway_route_to_public_subnets" {
   count                  = length(local.public_route_tables)
@@ -29,8 +35,7 @@ resource "aws_route" "internet_gateway_route_to_public_subnets" {
   gateway_id             = aws_internet_gateway.gw[0].id
 }
 
-
-resource "aws_route" "nat_gateway_route_to_public_subnets" {
+resource "aws_route" "nat_gateway_route_to_private_subnets" {
   count                  = length(local.private_route_tables)
   route_table_id         = element(local.private_route_tables, count.index)
   destination_cidr_block = "0.0.0.0/0"
