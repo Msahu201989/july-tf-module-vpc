ffcf737dbee04fe41ecb83e91f2af2c5e19c9683
Mukesh <Stunty@007>
2023-07-28 12:30:04 +0530

    July 23 learning devops

--- locals.tf
+++ locals.tf
@@ -1,15 +1,15 @@
-locals {
-  vpc_tags = {
-    Name    = "${var.env}-vpc"
-    ENV     = var.env
-    PROJECT = "roboshop"
-  }
-  vpc_peering_tags = {
-    Name    = "${var.env}-vpc-to-management-vpc"
-    ENV     = var.env
-    PROJECT = "roboshop"
-  }
-
-  all_route_tables = concat([for i, j in module.private_subnets : j.route_tables.id], [for i, j in module.public_subnets : j.route_tables.id])
-
-}
+#locals {
+#  vpc_tags = {
+#    Name    = "${var.env}-vpc"
+#    ENV     = var.env
+#    PROJECT = "roboshop"
+#  }
+#  vpc_peering_tags = {
+#    Name    = "${var.env}-vpc-to-management-vpc"
+#    ENV     = var.env
+#    PROJECT = "roboshop"
+#  }
+#
+#  all_route_tables = concat([for i, j in module.private_subnets : j.route_tables.id], [for i, j in module.public_subnets : j.route_tables.id])
+#
+#}
