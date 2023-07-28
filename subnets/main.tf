3ecffb890749839439661dbe288e5aeb9b58f9d6
Mukesh <Stunty@007>
2023-07-28 12:31:17 +0530

    July 23 learning devops

--- subnets/main.tf
+++ subnets/main.tf
@@ -15,14 +15,14 @@ resource "aws_route_table" "aws_route_table" {
   }
 }
 #
-#resource "aws_route_table_association" "route-table-association" {
-#  count          = length(aws_subnet.main)
-#  subnet_id      = element(aws_subnet.main.*.id, count.index)
-#  route_table_id = aws_route_table.aws_route_table.id
-#}
-#
-#resource "aws_route" "internet_gateway_route" {
-#  route_table_id         = aws_route_table.aws_route_table.id
-#  destination_cidr_block = "0.0.0.0/0"
-#  gateway_id             = var.gateway_id
-#}
+resource "aws_route_table_association" "route-table-association" {
+  count          = length(aws_subnet.main)
+  subnet_id      = element(aws_subnet.main.*.id, count.index)
+  route_table_id = aws_route_table.aws_route_table.id
+}
+
+resource "aws_route" "internet_gateway_route" {
+  route_table_id         = aws_route_table.aws_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = var.gateway_id
+}
